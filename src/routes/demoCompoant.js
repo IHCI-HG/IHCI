13a60e3fa12c1f7f777efcd58f8bca68156e353e
JinChengneng <jcn1024882812@gmail.com>
2017-05-07 15:29:54 +0800

    更改了routes中的todolist文件

--- src/routes/demoCompoant.js
+++ src/routes/demoCompoant.js
@@ -64,15 +64,4 @@ export const about = (store) => ({
   }
 })
 
-export const todolist = (store) => ({
-  path : 'todolist',
-  getComponent (nextState, cb) {
-    require.ensure([], (require) => {
-      const User = require('../containers/TodoList/containers/TodoListContainer').default
-      const reducer = require('../containers/TodoList/modules/todolist').default
-      injectReducer(store, { key: 'todolist', reducer })
-      cb(null, User)
-    }, 'user')
-  }
-})
 
