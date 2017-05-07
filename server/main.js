69d02dae559b9b3f4634402c4c81111c8a896ffb
arluber <lqygo1234@qq.com>
2017-05-07 23:26:30 +0800

    wechat + server优化

--- server/main.js
+++ server/main.js
@@ -208,6 +208,7 @@ app.use(compress())
 // ---------------------------------r---
 // Apply Webpack HMR Middleware
 // ------------------------------------
+
 if (project.env === 'development') {
     const compiler = webpack(webpackConfig)
     debug('Enabling webpack dev and HMR middleware')
@@ -220,25 +221,57 @@ if (project.env === 'development') {
         lazy: false,
         stats: project.compiler_stats
     }))
-    app.use(require('webpack-hot-middleware')(compiler))
-    // Serve static assets from ~/public since Webpack is unaware of
-    // these files. This middleware doesn't need to be enabled outside
-    // of development since this directory will be copied into ~/dist
-    // when the application is compiled.
+    app.use(require('webpack-hot-middleware')(compiler, {
+        path: '/__webpack_hmr'
+    }))
     app.use(express.static(project.paths.public()))
+    app.use('*', function (req, res, next) {
+        const filename = path.join(compiler.outputPath, 'index.html')
+        compiler.outputFileSystem.readFile(filename, (err, result) => {
+            if (err) {
+                return next(err)
+            }
+            res.set('content-type', 'text/html')
+            res.send(result)
+            res.end()
+        })
+    })
 } else {
-    debug(
-        'Server is being run outside of live development mode, meaning it will ' +
-        'only serve the compiled application bundle in ~/dist. Generally you ' +
-        'do not need an application server for this and can instead use a web ' +
-        'server such as nginx to serve your static files. See the "deployment" ' +
-        'section in the README for more information on deployment strategies.'
-    )
-    // Serving ~/dist by default. Ideally these files should be served by
-    // the web server and not the app server, but this helps to demo the
-    // server in production.
     app.use(express.static(project.paths.dist()))
 }
+
+
+// if (project.env === 'development') {
+//     const compiler = webpack(webpackConfig)
+//     debug('Enabling webpack dev and HMR middleware')
+//     app.use(require('webpack-dev-middleware')(compiler, {
+//         publicPath: webpackConfig.output.publicPath,
+//         contentBase: project.paths.client(),
+//         hot: true,
+//         quiet: project.compiler_quiet,
+//         noInfo: project.compiler_quiet,
+//         lazy: false,
+//         stats: project.compiler_stats
+//     }))
+//     app.use(require('webpack-hot-middleware')(compiler))
+//     // Serve static assets from ~/public since Webpack is unaware of
+//     // these files. This middleware doesn't need to be enabled outside
+//     // of development since this directory will be copied into ~/dist
+//     // when the application is compiled.
+//     app.use(express.static(project.paths.public()))
+// } else {
+//     debug(
+//         'Server is being run outside of live development mode, meaning it will ' +
+//         'only serve the compiled application bundle in ~/dist. Generally you ' +
+//         'do not need an application server for this and can instead use a web ' +
+//         'server such as nginx to serve your static files. See the "deployment" ' +
+//         'section in the README for more information on deployment strategies.'
+//     )
+//     // Serving ~/dist by default. Ideally these files should be served by
+//     // the web server and not the app server, but this helps to demo the
+//     // server in production.
+//     app.use(express.static(project.paths.dist()))
+// }
 
 module.exports = app
 
