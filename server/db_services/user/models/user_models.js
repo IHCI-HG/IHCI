656c4b04047ef6ec4007a08a0a1c15f6a81baa27
aferica <18819466397@qq.com>
2017-05-15 18:19:59 +0800

    重构server

---
+++ server/db_services/user/models/user_models.js
@@ -0,0 +1,18 @@
+/**
+ * Created by root on 17-1-4.
+ */
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    password: {type: String, default: '', trim: true},
+    username: {type: String, default: '', trim: true},
+    wechat: {type: String, default: '', trim: true},
+    phone: {type: String, default: '', trim: true},
+    email: {type: String, default: '', trim: true},
+    dataId: {type: String, default: '', trim: true},
+    isActive: {type: Number, default: 0, trim: true},
+    isTeacher: {type: Number, default: 0, trim: true}
+}, {collection: 'user'});
+
+exports.UserSchema = UserSchema;
