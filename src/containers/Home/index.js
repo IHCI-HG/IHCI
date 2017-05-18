a5e62bbf69be4d61e9326109b882160a9603af17
mary <mary@gmail.com>
2017-05-18 19:45:55 +0800

    修改主页

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => (
     <div className='jumbotron'>
-        <h1>这是主页</h1>
+        <h1>这是主页111111111</h1>
         <p>这是一些描述</p>
         <button className='btn btn-primary'>Learn more</button>
     </div>
