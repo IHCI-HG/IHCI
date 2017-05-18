76fa493fbad4d8adc89fc6d92ad7e99d74009be0
aferica <18819466397@qq.com>
2017-05-18 20:22:10 +0800

    删除无用console

--- src/routes/project.js
+++ src/routes/project.js
@@ -5,7 +5,6 @@ export const project = (store) => ({
     path: 'project',
     getComponent( nextState, cb) {
         require.ensure( [], (require) => {
-            console.log('project1')
             const component = require('../containers/project').default
             const reducer = require('../reducers/project').default
             injectReducer(store, { key: 'project', reducer })
