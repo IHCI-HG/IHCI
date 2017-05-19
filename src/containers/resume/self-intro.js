6e4d66d6e3c9516e997764817fe621d782df6259
arluber <lqygo1234@qq.com>
2017-05-19 19:56:57 +0800

    sss

--- src/containers/resume/self-intro.js
+++ src/containers/resume/self-intro.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
-// import Quill from '../../components/quill'
 
 class ResumeSelfIntro extends Component {
     componentDidMount() {
-        // var quill = new Quill('#editor-container', {
-        //     modules: {
-        //         toolbar: [
-        //             [{ header: [1, 2, false] }],
-        //             ['bold', 'italic', 'underline'],
-        //             ['image', 'code-block']
-        //         ]
-        //     },
-        //     placeholder: 'Compose an epic...',
-        //     theme: 'snow'  // or 'bubble'
-        // });
+
     }
 
     render() {
