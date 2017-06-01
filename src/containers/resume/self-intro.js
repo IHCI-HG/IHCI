1022b37e75335635f85abd0ee6e3c624d2ba4fc9
arluber <lqygo1234@qq.com>
2017-06-01 20:07:05 +0800

    fix

--- src/containers/resume/self-intro.js
+++ src/containers/resume/self-intro.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Input, Form, Button } from 'antd'
 const FormItem = Form.Item;
-
+import { browserHistory } from 'react-router'
 
 import './self-intro.scss'
 
@@ -16,7 +16,7 @@ class ResumeSelfIntro extends Component {
         return (
             <div className="self-intro-container">
                 <h1>自我介绍</h1>
-                <div className="description">在IHCI实验室，个人描述是你简历中最重要的部分，不过你没必要重复你在工作和项目经历中已经罗列出的内容。你可以畅所欲言，评价一下自己的技术能力、获奖经历或者一些能证明自己学习能力，我们希望从，聊聊自己的开源项目，理想中的团队，以及你的个人爱好等等。正是这些内容让你的简历看起来不再冰冷，而是充满了个人特色。</div>
+                <div className="description1">在IHCI实验室，个人描述是你简历中最重要的部分，不过你没必要重复你在工作和项目经历中已经罗列出的内容。你可以畅所欲言，评价一下自己的技术能力、获奖经历或者一些能证明自己学习能力，我们希望从，聊聊自己的开源项目，理想中的团队，以及你的个人爱好等等。正是这些内容让你的简历看起来不再冰冷，而是充满了个人特色。</div>
 
                 <Input
                     type="textarea"
@@ -34,9 +34,13 @@ class ResumeSelfIntro extends Component {
                     <Button type="primary" className="save-button" >
                         保存
                         </Button>
-                    <Button type="primary" className="next-page-button" >
+                    <Button
+                        type="primary"
+                        className="next-page-button"
+                        onClick={() => { browserHistory.push("/resume/project") }}
+                    >
                         下一页
-                        </Button>
+                    </Button>
                 </FormItem>
 
             </div>
